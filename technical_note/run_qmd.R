2e9de774ed512e660170242e1785295f897b61da
Karen Sanchez <karen.sanchez@conabio.gob.mx>
2026-05-07 11:17:03 -0600

    fix: add rproject path as parameter to technical note

--- technical_note/run_qmd.R
+++ technical_note/run_qmd.R
@@ -1,28 +1,36 @@
-df_anps <- read.csv('data/anp_cosmos/cosmos_anps.csv')
+# Run Quarto file to create technical notes
+library("here")
 
+# Create technical notes for cosmos project
+df_anps <- read.csv('data/anp_cosmos/cosmos_anps.csv')
 for (anp in df_anps$ANP_name){
   print(anp)
   if(anp != "Ciénegas del Lerma") {
     anp_shortname <- df_anps[df_anps$ANP_name == anp, "ANP_shortname"]
     quarto::quarto_render(
-      "/Users/kasanchez/Documents/ie_dashboard/technical_note/ficha.qmd",
+      input = "technical_note/ficha.qmd",
+      execute_dir = here(),
       output_file = paste0("ficha_",anp_shortname,".pdf"),
-      execute_params = list(anp_name= anp, 
-                            project_name = "cosmos")
+      execute_params = list(rproject_path = here(),
+                            project_name = "cosmos",
+                            anp_name= anp
+                            )
     )
   }
 }
 
+# Create technical notes for sym project
 df_anps <- read.csv('data/anp_sym/sym_anps.csv')
 for (anp in df_anps$ANP_name){
   print(anp)
   anp_shortname <- df_anps[df_anps$ANP_name == anp, "ANP_shortname"]
   quarto::quarto_render(
-    "/Users/kasanchez/Documents/ie_dashboard/technical_note/ficha.qmd",
+    input = "technical_note/ficha.qmd",
+    execute_dir = here(),
     output_file = paste0("ficha_",anp_shortname,".pdf"),
-    execute_params = list(anp_name= anp, 
-                          project_name = "sym")
+    execute_params = list(rproject_path = here(),
+                          project_name = "other",
+                          anp_name= anp
+                          )
   )
 }
-
-
