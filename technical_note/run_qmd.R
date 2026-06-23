703a1fa6b0e73295433055f0cafc7b14ea67669f
Karen Sanchez <karen.sanchez@conabio.gob.mx>
2026-06-23 13:25:18 -0600

    refactor: rendering function for technical notes

--- technical_note/run_qmd.R
+++ technical_note/run_qmd.R
@@ -1,21 +1,31 @@
 # Run Quarto file to create technical notes
 library("here")
 
+render_ficha <- function(anp, anp_shortname, project_name, header_file) {
+  header_path <- normalizePath(file.path(here(), header_file), mustWork = TRUE)
+
+  quarto::quarto_render(
+    input = "technical_note/ficha.qmd",
+    execute_dir = here(),
+    output_file = paste0("ficha_", anp_shortname, ".pdf"),
+    execute_params = list(rproject_path = here(),
+                          project_name = project_name,
+                          anp_name = anp
+                          ),
+    pandoc_args = c(paste0("--include-in-header=", header_path))
+  )
+}
+
 # Create technical notes for cosmos project
 df_anps <- read.csv('data/anp_cosmos/cosmos_anps.csv')
 for (anp in df_anps$ANP_name){
   print(anp)
   if(anp != "Ciénegas del Lerma") {
     anp_shortname <- df_anps[df_anps$ANP_name == anp, "ANP_shortname"]
-    quarto::quarto_render(
-      input = "technical_note/ficha.qmd",
-      execute_dir = here(),
-      output_file = paste0("ficha_",anp_shortname,".pdf"),
-      execute_params = list(rproject_path = here(),
-                            project_name = "cosmos",
-                            anp_name= anp
-                            )
-    )
+    render_ficha(anp,
+                 anp_shortname,
+                 "cosmos",
+                 "technical_note/header_cosmos.tex")
   }
 }
 
@@ -24,13 +34,8 @@ df_anps <- read.csv('data/anp_sym/sym_anps.csv')
 for (anp in df_anps$ANP_name){
   print(anp)
   anp_shortname <- df_anps[df_anps$ANP_name == anp, "ANP_shortname"]
-  quarto::quarto_render(
-    input = "technical_note/ficha.qmd",
-    execute_dir = here(),
-    output_file = paste0("ficha_",anp_shortname,".pdf"),
-    execute_params = list(rproject_path = here(),
-                          project_name = "other",
-                          anp_name= anp
-                          )
-  )
+  render_ficha(anp,
+               anp_shortname,
+               "other",
+               "technical_note/header_sym.tex")
 }
